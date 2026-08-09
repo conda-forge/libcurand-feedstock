71192c02c49ef739d51efb79944ceb86f487a2fc
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-08-09 20:32:14 +0200

    Fix Windows staging directories

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,8 @@
-if not exist %PREFIX% mkdir %PREFIX%
-if not exist %LIBRARY_LIB%\x64 mkdir %LIBRARY_LIB%\x64
+if not exist "%LIBRARY_LIB%\x64" mkdir "%LIBRARY_LIB%\x64"
+if not exist "%LIBRARY_BIN%" mkdir "%LIBRARY_BIN%"
+if not exist "%LIBRARY_INC%" mkdir "%LIBRARY_INC%"
 
-move lib\x64\* %LIBRARY_LIB%\x64
-move bin\x64\* %LIBRARY_BIN%
-move include\* %LIBRARY_INC%
+move lib\x64\* "%LIBRARY_LIB%\x64"
+move bin\x64\* "%LIBRARY_BIN%"
+move include\* "%LIBRARY_INC%"
 
